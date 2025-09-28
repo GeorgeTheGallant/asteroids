275447f990dd1860ab61340684fcddde10b92a91
GeorgeTheGallant <djordje.a97@gmail.com>
2025-09-28 11:22:18 +0200

    feature: add time tracking to restrict FPS

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from constants import *
 
 def main():
     pygame.init()
-    
-    print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +16,8 @@ def main():
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
